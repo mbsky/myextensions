39deadc6e9a8f61721856d3d3d8dbbbc98b8b51d
MvcWidgets <MvcWidgets@95f74a22-d9d4-11dd-8772-d1b041bf0e37>
2009-01-09 03:07:50 +0000

    Added XmlResult, XmlViewResult, XDocumentResult and ControllerEx XmlResults Extensions.

---
+++ trunk/MyExtensions.Web.Mvc/Results/XmlViewResult.cs
@@ -0,0 +1,32 @@
+﻿
+namespace System.Web.Mvc
+{
+    /// <summary>
+    /// return a ViewResult with "text/xml" ContentType,this result requires a friendly XmlTempalte ViewPage.
+    /// </summary>
+    public class XmlViewResult : ViewResult
+    {
+        private string viewName;
+
+        public XmlViewResult() : this("") { }
+
+        public XmlViewResult(string viewName)
+            : base()
+        {
+            this.viewName = viewName;
+        }
+
+        public override void ExecuteResult(ControllerContext context)
+        {
+            if (!string.IsNullOrEmpty(viewName))
+                ViewName = viewName;
+
+            TempData = context.Controller.TempData;
+            ViewData = context.Controller.ViewData;
+
+            base.ExecuteResult(context);
+
+            context.HttpContext.Response.ContentType = "text/xml";
+        }
+    }
+}
