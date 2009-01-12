1cb0b7623b80aee1e828ff7f7951015db87dc165
MvcWidgets <MvcWidgets@95f74a22-d9d4-11dd-8772-d1b041bf0e37>
2009-01-12 03:46:48 +0000

    added /maxcpucount option to msbuild.reg.

--- MyExtensions.Web.Mvc/ControllerEx.XmlResults.cs
+++ MyExtensions.Web.Mvc/ControllerEx.XmlResults.cs
@@ -4,6 +4,13 @@ namespace System.Web.Mvc
 {
     public abstract partial class ControllerEx
 	{
+        protected virtual XmlResult Xml(object data)
+        {
+            Check.AssertNotNullOrEmpty(data,"data");
+
+            return new XmlResult(data, data.GetType());
+        }
+
         protected virtual XmlViewResult Xml()
         {
             return Xml("");
