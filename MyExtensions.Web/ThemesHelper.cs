40caf56b6c3e24b5335597704e8fd4d902c83324
MvcWidgets <MvcWidgets@95f74a22-d9d4-11dd-8772-d1b041bf0e37>
2009-02-16 05:05:06 +0000

    Added CookieHelper ThemesHelper.

---
+++ MyExtensions.Web/ThemesHelper.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+
+namespace System.Web
+{
+    public class ThemesHelper
+    {
+        AppSettingsHelper appSettings = new AppSettingsHelper();
+
+        public virtual string[] Themes
+        {
+            get
+            {
+                return appSettings.GetArray("Themes");
+            }
+        }
+
+        //public virtual string[] UserThemes
+        //{
+        //    get
+        //    {
+        //        return appSettings.GetArray("UserThemes");
+        //    }
+        //}
+
+        //public virtual string[] MasterThemes
+        //{
+        //    get
+        //    {
+        //        return appSettings.GetArray("MasterThemes");
+        //    }
+        //}
+
+        //public virtual string[] CssThemes
+        //{
+        //    get
+        //    {
+        //        return appSettings.GetArray("CssThemes");
+        //    }
+        //}
+
+        public virtual string DefaultTheme
+        {
+            get
+            {
+                string theme = appSettings.Get("Theme");
+
+                if (theme.IsNullOrEmpty())
+                {
+                    theme = appSettings.Get("DefaultTheme");
+                }
+
+                return theme;
+            }
+        }
+
+        public virtual string Theme
+        {
+            get
+            {
+                string val = CookieHelper.GetCookieValue("theme");
+
+                if (!val.IsNullOrEmpty())
+                    return val;
+
+                return DefaultTheme;
+            }
+            set
+            {
+                CookieHelper.SetCookie("theme", value, true);
+            }
+        }
+    }
+}
