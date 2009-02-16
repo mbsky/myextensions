eefe2700eca2ed6bd2ab3f853295b557bfde8196
MvcWidgets <MvcWidgets@95f74a22-d9d4-11dd-8772-d1b041bf0e37>
2009-02-16 05:48:59 +0000

    Added IViewDataContainerEx,ViewMasterPageEx,ViewPageEx.

---
+++ MyExtensions.Web.Mvc/IViewDataContainerExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Security;
+using System.Configuration;
+
+namespace System.Web.Mvc
+{
+    public interface IViewDataContainerEx
+    {
+        ViewContext ViewContext { get;}
+    }
+
+    public static class IViewDataContainerExExtensions
+    {
+        public static bool GetIsDebugging(this IViewDataContainerEx view)
+        {
+            if (view.ViewContext.Controller is ControllerEx)
+                return ((ControllerEx)view.ViewContext.Controller).IsDebugging;
+            else
+                return false;
+        }
+
+        public static AppSettingsHelper GetAppSettings(this IViewDataContainerEx view)
+        {
+            if (view.ViewContext.Controller is ControllerEx)
+                return ((ControllerEx)view.ViewContext.Controller).AppSettings;
+            else
+                return null;
+        }
+    }
+}
