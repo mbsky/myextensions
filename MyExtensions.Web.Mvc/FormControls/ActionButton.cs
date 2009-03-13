471852af5b8096f3c0ecf4e5b7212c78cbe57bea
MvcWidgets <MvcWidgets@95f74a22-d9d4-11dd-8772-d1b041bf0e37>
2009-03-13 01:03:09 +0000

    Added AutoResult.

---
+++ MyExtensions.Web.Mvc/FormControls/ActionButton.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Routing;
+
+namespace System.Web.Mvc.Html
+{
+    public class ActionButton : BaseActionCaller
+    {
+
+        public ActionButton(string value)
+        {
+            Check.AssertNotNullOrEmpty(value, "value");
+            Value = value;
+        }
+
+        //public virtual object RouteValues { get; set; }
+
+        public virtual string Value { get; set; }
+
+        public override FormMethod Method
+        {
+            get
+            {
+                return FormMethod.Post;
+            }
+        }
+    }
+
+    public static class ActionButtonExtensions
+    {
+
+        static readonly string defaultTemplateName = "ActionButton";
+
+        public static void RenderActionButton(this HtmlHelper helper, ActionButton model)
+        {
+
+            string templateName = defaultTemplateName;
+
+            if (model.ControllerName.IsNullOrEmpty())
+            {
+                model.ControllerName = (string)helper.ViewContext.RouteData.Values["controller"];
+            }
+
+            if (model.ActionName.IsNullOrEmpty())
+            {
+                model.ActionName = (string)helper.ViewContext.RouteData.Values["action"];
+            }
+
+            helper.RenderPartial(templateName, model, helper.ViewDataContainer.ViewData);
+        }
+    }
+}
---
+++ MyExtensions.Web.Mvc/FormControls/BaseActionCaller.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace System.Web.Mvc.Html
+{
+    public abstract class BaseActionCaller : System.Web.Mvc.Html.IActionCaller
+    {
+        public virtual string ActionName { get; set; }
+        public virtual bool Ajax { get; set; }
+        public virtual bool AntiForgeryToken { get; set; }
+        public virtual string ControllerName { get; set; }
+        public virtual System.Web.Mvc.FormMethod Method { get; set; }
+    }
+}
